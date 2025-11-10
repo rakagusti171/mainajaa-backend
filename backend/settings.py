233433346a7e10cc2c69e8a01d2d515ed96755a5
PabloRaka <ldeath665@gmail.com>
2025-11-10 11:45:59 +0700

    update emali backend

--- backend/settings.py
+++ backend/settings.py
@@ -273,13 +273,13 @@ EXCHANGERATE_API_KEY = os.environ.get('EXCHANGERATE_API_KEY', '')  # Optional: f
 # ======================================================
 # EMAIL CONFIG
 # ======================================================
-EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER or 'noreply@mainajaa.com'
 
 # ======================================================
 # SECURITY (PRODUCTION)
